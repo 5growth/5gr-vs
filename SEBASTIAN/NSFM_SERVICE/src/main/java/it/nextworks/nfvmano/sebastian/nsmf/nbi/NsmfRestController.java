6648f2c7d05b1e123c320b39481d0f7d1eca3be1
j.brenes <j.brenes@nextworks.it>
2020-10-06 20:19:50 +0200

    Fixed rest endpoint

--- SEBASTIAN/NSFM_SERVICE/src/main/java/it/nextworks/nfvmano/sebastian/nsmf/nbi/NsmfRestController.java
+++ SEBASTIAN/NSFM_SERVICE/src/main/java/it/nextworks/nfvmano/sebastian/nsmf/nbi/NsmfRestController.java
@@ -3,8 +3,12 @@ package it.nextworks.nfvmano.sebastian.nsmf.nbi;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import it.nextworks.nfvmano.libs.ifa.common.elements.Filter;
 import it.nextworks.nfvmano.libs.ifa.common.exceptions.MalformattedElementException;
@@ -31,7 +30,7 @@ import it.nextworks.nfvmano.sebastian.nsmf.messages.InstantiateNsiRequest;
 import it.nextworks.nfvmano.sebastian.nsmf.messages.ModifyNsiRequest;
 import it.nextworks.nfvmano.sebastian.nsmf.messages.TerminateNsiRequest;
 import it.nextworks.nfvmano.sebastian.record.elements.NetworkSliceInstance;
-@Api(tags = "Network Slice Management API")
+@Api(tags = "Network Slice LCM API")
 @RestController
 @CrossOrigin
 @RequestMapping("/vs/basic/nslcm")
@@ -79,7 +78,17 @@ public class NsmfRestController {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
+
+	@ApiOperation(value = "Get the Network Slice Instance with the specified ID ")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "The Network Slice Instance.", response = NetworkSliceInstance.class),
+			//@ApiResponse(code = 400, message = "The request contains elements impossible to process", response = ResponseEntity.class),
+			//@ApiResponse(code = 409, message = "There is a conflict with the request", response = ResponseEntity.class),
+			//@ApiResponse(code = 500, message = "Status 500", response = ResponseEntity.class)
+
+	})
+	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/ns/{nsiId}", method = RequestMethod.GET)
 	public ResponseEntity<?> getNsInstance(@PathVariable String nsiId, Authentication auth) {
 		log.debug("Received query for network slice instance with ID " + nsiId);
@@ -103,7 +112,16 @@ public class NsmfRestController {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
+	@ApiOperation(value = "Get all the Network Slice Instances")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "The list of Network Slice Instances.", response = NetworkSliceInstance.class, responseContainer = "Set"),
+			//@ApiResponse(code = 400, message = "The request contains elements impossible to process", response = ResponseEntity.class),
+			//@ApiResponse(code = 409, message = "There is a conflict with the request", response = ResponseEntity.class),
+			//@ApiResponse(code = 500, message = "Status 500", response = ResponseEntity.class)
+
+	})
+	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/ns", method = RequestMethod.GET)
 	public ResponseEntity<?> getNsInstance(Authentication auth) {
 		log.debug("Received query for all network slice instances.");
@@ -114,6 +132,37 @@ public class NsmfRestController {
 			GeneralizedQueryRequest query = new GeneralizedQueryRequest(filter, null);
 			List<NetworkSliceInstance> nsis = nsLcmService.queryNetworkSliceInstance(query, localDomainId, tenantId);
 			return new ResponseEntity<List<NetworkSliceInstance>>(nsis, HttpStatus.OK);	
+		} catch (MalformattedElementException e) {
+			log.error("NS ID creation failed due to bad-formatted request.");
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		} catch (Exception e) {
+			log.error("NS ID creation failed due to internal errors.");
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@ApiOperation(value = "Get all the Network Slice Instance IDSs")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "The list of Network Slice Instances IDS.", response = String.class, responseContainer = "Set"),
+			//@ApiResponse(code = 400, message = "The request contains elements impossible to process", response = ResponseEntity.class),
+			//@ApiResponse(code = 409, message = "There is a conflict with the request", response = ResponseEntity.class),
+			//@ApiResponse(code = 500, message = "Status 500", response = ResponseEntity.class)
+
+	})
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "/ns/nsinstanceids", method = RequestMethod.GET)
+	public ResponseEntity<?> getNsInstanceIds(Authentication auth) {
+		log.debug("Received query for all network slice instances.");
+		try {
+			String tenantId = getUserFromAuth(auth);
+			Map<String, String> parameters = new HashMap<String, String>();
+			Filter filter = new Filter(parameters);
+			GeneralizedQueryRequest query = new GeneralizedQueryRequest(filter, null);
+			List<String> nsis = nsLcmService.queryNetworkSliceInstance(query, localDomainId, tenantId).stream()
+					.map(nsInstance -> nsInstance.getNsiId())
+					.collect(Collectors.toList());
+
+			return new ResponseEntity<List<String>>(nsis, HttpStatus.OK);
 		} catch (MalformattedElementException e) {
 			log.error("NS ID creation failed due to bad-formatted request.");
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
